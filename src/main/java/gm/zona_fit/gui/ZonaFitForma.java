e4cd3770aa1bab52685ab9fc27e61412abc9d174
bitlogic <nicolas.castro@bitlogic.io>
2025-03-06 14:54:51 -0300

    agregando funcionalidades

--- src/main/java/gm/zona_fit/gui/ZonaFitForma.java
+++ src/main/java/gm/zona_fit/gui/ZonaFitForma.java
@@ -1,5 +1,6 @@
 package gm.zona_fit.gui;
 
+import gm.zona_fit.modeloEntity.Cliente;
 import gm.zona_fit.servicio.ClienteServicio;
 import gm.zona_fit.servicio.IClienteServicio;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,10 @@ import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 @Component
 public class ZonaFitForma extends JFrame {
@@ -20,6 +25,7 @@ public class ZonaFitForma extends JFrame {
     private JButton limpiarButton;
     IClienteServicio clienteServicio;
     private DefaultTableModel tablaModeloClientes;
+    private Integer idCliente;
 
     //Constructor
     @Autowired
@@ -27,6 +33,14 @@ public class ZonaFitForma extends JFrame {
         //Inyección de dependencias
         this.clienteServicio = clienteServicio;
         iniciarForma();
+        guardarButton.addActionListener(e -> guardarCliente());
+        clientesTabla.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                cargarClienteSeleccionado();
+            }
+        });
     }
 
     public void iniciarForma(){
@@ -35,7 +49,6 @@ public class ZonaFitForma extends JFrame {
         setSize(900,700);
         setLocationRelativeTo(null);
     }
-
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
@@ -55,6 +68,50 @@ public class ZonaFitForma extends JFrame {
             Object[] reglonCliente = {cliente.getId(), cliente.getNombre(), cliente.getApellido(), cliente.getMembresia()};
             this.tablaModeloClientes.addRow(reglonCliente);
         });
+    }
+
+    private void guardarCliente(){
+        if(nombreTexto.getText().equals("")){
+            mostrarMensaje("El nombre es requerido");
+            nombreTexto.requestFocusInWindow();
+            return;
+        }
+        if(membresiaTexto.getText().equals("")){
+            mostrarMensaje("La membresia es requerida");
+            membresiaTexto.requestFocusInWindow();
+            return;
+        }
+        //Recuperar los valores del formulario
+        var nombre = nombreTexto.getText();
+        var apellido = apellidoTexto.getText();
+        var membresia = Integer.parseInt(membresiaTexto.getText());
+        //Guardar el cliente
+        var cliente = new Cliente();
+        cliente.setNombre(nombre);
+        cliente.setApellido(apellido);
+        cliente.setMembresia(membresia);
+        // inserta:
+        this.clienteServicio.guardarCliente(cliente);
+        limpiarFormulario();
+        listarClientes();
+    }
+
+    private void cargarClienteSeleccionado(){
+        var renglon = clientesTabla.getSelectedRow();
+        if(renglon != -1){
+            var id = clientesTabla.getModel().getValueAt(renglon, 0).toString();
+
+        }
+    }
+
+    private void mostrarMensaje(String mensaje){
+        JOptionPane.showMessageDialog(this, mensaje);
+    }
+
+    private void limpiarFormulario(){
+        nombreTexto.setText("");
+        apellidoTexto.setText("");
+        membresiaTexto.setText("");
     }
 
 }
